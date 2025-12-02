6c108e1049e9d3010feaa39c99c94105c3033584
mitigd <mitigatingdiscord@proton.me>
2025-12-01 20:50:36 -0600

    remove unused var

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import Chart from 'chart.js/auto';
 
 // --- Theme Constants ---
